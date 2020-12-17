335476998a05d6769a3938212ef95818f54c489b
Rayan Ahmedy <rayanahmedy1999@gmail.com>
2020-12-17 01:40:58 -0500

    Created react app and deleted unneccisary files

---
+++ src/App.js
@@ -0,0 +1,15 @@
+import React from 'react';
+import './App.css';
+
+
+function App() {
+  return(
+    <div className='App'>
+      <h1>What is up</h1>
+    </div>
+  )
+
+
+}
+
+export default App;
