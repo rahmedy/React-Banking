829664643255c3cca6c4b7c028b5494cee46880e
Rayan Ahmedy <rayanahmedy1999@gmail.com>
2020-12-17 18:12:53 -0500

    added styled component packages and put the Nav in app

--- src/App.js
+++ src/App.js
@@ -1,12 +1,14 @@
 import React from 'react';
 import './App.css';
+import Navbar from './components/Navbar';
+import { BrowserRouter as Router} from 'react-router-dom'
 
 
 function App() {
   return(
-    <div className='App'>
-      <h1>What is up</h1>
-    </div>
+    <Router>
+      <Navbar />
+    </Router>
   )
 
 
