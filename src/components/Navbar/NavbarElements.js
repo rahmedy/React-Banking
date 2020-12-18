760c9e096c0bb2446de713847d6f5376b6da622b
Rayan Ahmedy <rayanahmedy1999@gmail.com>
2020-12-18 01:28:51 -0500

    styled navabr compoentents and added FaBars

--- src/components/Navbar/NavbarElements.js
+++ src/components/Navbar/NavbarElements.js
@@ -1,6 +1,6 @@
 import styled from 'styled-components'
 import { Link as LinkR } from 'react-router-dom'
-// import { Link as LinkS } from 'react-scroll'
+import { Link as LinkS } from 'react-scroll'
 
 
 
@@ -48,7 +48,44 @@ export const Nav = styled.nav`
 
         @media screen and (max-width: 768px) {
             display: block;
+            position: absolute;
+            top: 0;
+            right: 0; 
+            transform: translate(-100%, 60%);
+            font-size: 1.8rem;
+            cursor: pointer;
+            color: #fff;
             
-        }
-
+      }
     `
+
+    export const NavMenu = styled.ul`
+      display: flex;
+      align-items: center;
+      list-style: none;
+      text-align: center;
+      margin-right: -22px;
+
+      @media screen and (max-width: 768px) {
+          display: none;
+      }
+    `
+
+    export const NavItem = styled.li`
+      height: 80px;
+    `
+
+    export const NavLinks = styled(LinkS)`
+       color: #fff;
+       display: flex;
+       align-items: center;
+       text-decoration: none;
+       padding: 0 1rem;
+       height: 100%;
+       cursor: pointer;
+
+
+       &.active {
+           border-bottom: 3px solid #01bf71;
+       }
+    `;
