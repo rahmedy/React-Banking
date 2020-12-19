05e18479b32b7568c47d5a94c53817b33852dbd6
Rayan Ahmedy <rayanahmedy1999@gmail.com>
2020-12-18 21:28:21 -0500

    Styled the sidebar after click

--- src/App.js
+++ src/App.js
@@ -1,13 +1,13 @@
 import React from 'react';
 import './App.css';
-import Navbar from './components/Navbar';
+import Home from './pages'
 import { BrowserRouter as Router} from 'react-router-dom'
 
 
 function App() {
   return(
     <Router>
-      <Navbar />
+      <Home />
     </Router>
   )
 
---
+++ src/pages/index.js
@@ -0,0 +1,18 @@
+import React from 'react'
+
+import Sidebar from '../components/Sidebar';
+import Navbar from '../components/Navbar';
+
+
+
+const Home = () => {
+    return (
+        <>  
+            <Sidebar />
+            <Navbar />
+           
+        </>
+    )
+}
+
+export default Home
