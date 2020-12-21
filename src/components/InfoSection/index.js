aed87135b31d483be6ecbec250ded4ad3f163b98
Rayan Ahmedy <rayanahmedy1999@gmail.com>
2020-12-20 21:47:41 -0500

    imported info elements to the info section

--- src/components/InfoSection/index.js
+++ src/components/InfoSection/index.js
@@ -1,27 +1,46 @@
-import React from 'react'
-import { Button } from 'react-scroll'
+import React from 'react';
+import { Button } from 'react-scroll';
+import {
+	Column2,
+	ImgWrap,
+	InfoContainer,
+	InfoWrapper,
+	InfoRow,
+	Column1,
+	TextWrapper,
+	TopLine,
+	Heading,
+	Subtitle,
+	BtnWrap,
+	Img
+} from './InfoElements';
 
 const InfoSection = () => {
-    return (
+	return (
         <>
-          <InfoContainer id={id}>
-              <InfoWrapper>
-                  <InfoRow>
-                      <Column1>
-                      <TextWrapper>
-                          <TopLine>Top line</TopLine>
-                          <Heading>heafding</Heading>
-                          <Subtitle>subtitle</Subtitle>
-                          <BtnWrap>
-                              <Button to='home'>Button</Button>
-                              </BtnWrap>
-                      </TextWrapper>
-                      </Column1>
-                  </InfoRow>
-              </InfoWrapper>
-              </InfoContainer>  
+		<InfoContainer>
+			<InfoWrapper>
+				<InfoRow>
+					<Column1>
+						<TextWrapper>
+							<TopLine>Top line</TopLine>
+							<Heading>heafding</Heading>
+							<Subtitle>subtitle</Subtitle>
+							<BtnWrap>
+								<Button to="home" />
+							</BtnWrap>
+						</TextWrapper>
+					</Column1>
+					<Column2>
+						<ImgWrap>
+							<Img />
+						</ImgWrap>
+					</Column2>
+				</InfoRow>
+			</InfoWrapper>
+		</InfoContainer>
         </>
-    )
-}
+	);
+};
 
-export default InfoSection
+export default InfoSection;
